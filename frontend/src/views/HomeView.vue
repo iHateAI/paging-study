96a399ecbae29013060a89264245ac5cc806f421
HyunSeong <hhs950120@gmail.com>
2022-09-29 14:17:34 +0900

    FEAT: 페이지 변경 버튼 구현 getPreviosPage 함수 getNextPage 함수

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,24 +1,33 @@
 <template>
   <div class="home">
     <h1>게시판 페이징 기능 구현</h1>
-    <table class="board">
-      <thead>
-        <tr>
-          <th>번호</th>
-          <th>제목</th>
-          <th>작성자</th>
-          <th>등록일</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="post in postList" :key="post.id">
-          <td>{{post.id}}</td>
-          <td>{{post.title}}</td>
-          <td>{{post.author}}</td>
-          <td>{{post.createDate}}</td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="board-container">
+      <table class="board">
+        <thead>
+          <tr>
+            <th>번호</th>
+            <th>제목</th>
+            <th>작성자</th>
+            <th>등록일</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="post in postList" :key="post.id">
+            <td>{{post.id}}</td>
+            <td>{{post.title}}</td>
+            <td>{{post.author}}</td>
+            <td>{{post.created_at}}</td>
+          </tr>
+        </tbody>
+      </table>
+      <div class="seq">
+        <button @click="getPreviosPage">이전</button>
+        <span>{{pageNum}}</span>
+        /
+        <span>15</span>
+        <button @click="getNextPage">다음</button>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -33,22 +42,43 @@ export default {
   },
   data() {
     return {
-      postList: [
-        {id: 1, title: '제목 1', author: '익명 1', createDate: '2020-12-02'}, 
-        {id: 2, title: '제목 2', author: '익명 2', createDate: '2020-12-02'}, 
-        {id: 3, title: '제목 3', author: '익명 3', createDate: '2020-12-02'}, 
-        {id: 4, title: '제목 4', author: '익명 4', createDate: '2020-12-02'}
-      ]
+      pageNum: 1,
+      pageSize: 5,
+      maxPage: null,
+      postList: []
     }
   },
-  created() {
-    axios.get('http://localhost:3000/boards?pageNum=1&pageSize=5', {withCredentials: true})
+  methods: {
+    getPreviosPage: function() {
+      if (this.pageNum !== 1)
+        this.pageNum--;
+      axios.get(`http://localhost:3000/boards?pageNum=${this.pageNum}&pageSize=5`)
       .then(res => {
-        console.log(res.data);
+        this.postList = res.data;
       })
       .catch(err => {
         console.error(err);
+      });
+    },
+    getNextPage: function() {
+      this.pageNum++;
+      axios.get(`http://localhost:3000/boards?pageNum=${this.pageNum}&pageSize=5`)
+      .then(res => {
+        this.postList = res.data;
       })
+      .catch(err => {
+        console.error(err);
+      });
+    }
+  }, 
+  created() {
+    axios.get('http://localhost:3000/boards?pageNum=1&pageSize=5')
+      .then(res => {
+        this.postList = res.data;
+      })
+      .catch(err => {
+        console.error(err);
+      });
   },
 }
 </script>
